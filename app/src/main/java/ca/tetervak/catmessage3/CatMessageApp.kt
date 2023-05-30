bd0ed015b38bc79bd327ba96b7c7c612e032575f
Alex Tetervak <tetervak@sheridancollege.ca>
2023-05-30 05:24:49 -0400

    used strings in routes explicitly

--- app/src/main/java/ca/tetervak/catmessage3/CatMessageApp.kt
+++ app/src/main/java/ca/tetervak/catmessage3/CatMessageApp.kt
@@ -1,9 +1,6 @@
 package ca.tetervak.catmessage3
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavArgument
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -21,22 +18,22 @@ fun CatMessageApp() {
         val navController = rememberNavController()
         NavHost(
             navController = navController,
-            startDestination = INPUT
+            startDestination = "input"
         ) {
-            composable(route = INPUT) {
+            composable(route = "input") {
                 InputScreen(
                     onSend = { urgent, catMessage ->
                         navController.navigate(
-                            route = "$OUTPUT/$catMessage?$URGENCY_ARG=$urgent"
+                            route = "output/$catMessage?urgent=$urgent"
                         )
                     }
                 )
             }
             composable(
-                route = OUTPUT_WITH_ARGS,
+                route = "output/{catMessage}?urgent={urgent}",
                 arguments = listOf(
-                    navArgument(CAT_MESSAGE_ARG){ type = NavType.StringType},
-                    navArgument(URGENCY_ARG){
+                    navArgument("catMessage"){ type = NavType.StringType},
+                    navArgument("urgent"){
                         type = NavType.BoolType
                         defaultValue = false
                     }
@@ -44,8 +41,8 @@ fun CatMessageApp() {
             ) { backStackEntry ->
                 val arguments = backStackEntry.arguments!!
                 OutputScreen(
-                    urgent = arguments.getBoolean(URGENCY_ARG),
-                    catMessage = CatMessage.valueOf(arguments.getString(CAT_MESSAGE_ARG)!!),
+                    urgent = arguments.getBoolean("urgent"),
+                    catMessage = CatMessage.valueOf(arguments.getString("catMessage")!!),
                     onBack = {
                         navController.popBackStack()
                     }
--- app/src/main/java/ca/tetervak/catmessage3/NavRouts.kt
+++
@@ -1,8 +0,0 @@
-package ca.tetervak.catmessage3
-
-const val INPUT = "input"
-const val OUTPUT = "output"
-
-const val URGENCY_ARG = "urgent"
-const val CAT_MESSAGE_ARG = "catMessage"
-const val OUTPUT_WITH_ARGS="$OUTPUT/{$CAT_MESSAGE_ARG}?$URGENCY_ARG={$URGENCY_ARG}"
