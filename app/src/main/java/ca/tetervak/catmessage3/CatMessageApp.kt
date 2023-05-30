5a47dd36d6c5fba07e35c0aa215519c1b44c5600
Alex Tetervak <tetervak@sheridancollege.ca>
2023-05-29 22:54:16 -0400

    migration to m3

--- app/src/main/java/ca/tetervak/catmessage3/CatMessageApp.kt
+++ app/src/main/java/ca/tetervak/catmessage3/CatMessageApp.kt
@@ -1,7 +1,7 @@
 package ca.tetervak.catmessage3
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
